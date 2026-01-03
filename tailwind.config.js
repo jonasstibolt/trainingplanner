140cf6253347bb4ebab0a84dfbc05e513d0cba5a
jonasstibolt <jonasholmgaard@gmail.com>
2026-01-03 08:45:57 +0100

    installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./plans/templates/**/*.html",
+    "./**/templates/**/*.html",
+    "./**/forms.py",
+    "./**/views.py",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
